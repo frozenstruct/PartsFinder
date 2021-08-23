f0967a2ada36832082cdd3f3e9f50427dca311b6
Аксёнов Дмитрий Александрович <DAlAksenov@sberbank.ru>
2021-08-23 13:21:28 +0300

    Initial Commit

---
+++ PartsFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartsFinder
+//
+//  Created by 19268356 on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
