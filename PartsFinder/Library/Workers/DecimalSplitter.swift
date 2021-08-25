27811712e45efd9d8e9cc935f28a496bc1bda793
Аксёнов Дмитрий Александрович <DAlAksenov@sberbank.ru>
2021-08-25 18:11:10 +0300

    reached known good state

--- PartsFinder/Library/Workers/DecimalSplitter.swift
+++ PartsFinder/Library/Workers/DecimalSplitter.swift
@@ -9,15 +9,34 @@ import Foundation
 
 protocol DecimalSplitterProtocol {
 	
-	func split(_ input: Double) -> (part0: Double, part1: Double)
+	func split(_ input: Double) -> (part0: NSDecimalNumber, part1: NSDecimalNumber)
 }
 
 final class DecimalSplitter: DecimalSplitterProtocol {
 	
-	func split(_ input: Double) -> (part0: Double, part1: Double) {
+	func split(_ input: Double) -> (part0: NSDecimalNumber, part1: NSDecimalNumber) {
 		let randomUpperBound = Double.random(in: 0.4...0.5)
 		let random = Double.random(in: 0...Double(input) * randomUpperBound)
-		
-		return (random, input - random)
+
+		let part0 = NSDecimalNumber(value: random)
+		let part1 = NSDecimalNumber(value: input - random)
+
+		return roundTwoPlaces((part0, part1))
+	}
+
+	func roundTwoPlaces(_ input: SplitResultEntity) -> SplitResultEntity {
+		let roundingBehavior = NSDecimalNumberHandler(
+			roundingMode: .plain,
+			scale: 2,
+			raiseOnExactness: false,
+			raiseOnOverflow: false,
+			raiseOnUnderflow: false,
+			raiseOnDivideByZero: false
+		)
+
+		return (
+			input.part0.rounding(accordingToBehavior: roundingBehavior),
+			input.part1.rounding(accordingToBehavior: roundingBehavior)
+		)
 	}
 }
