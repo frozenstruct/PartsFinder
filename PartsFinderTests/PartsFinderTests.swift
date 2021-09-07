ff9931aae498316e718abb2ac5b2103ed85904f8
Dima <dmi.aksyonov@gmail.com>
2021-09-07 11:50:22 +0300

    removed some clutter

--- PartsFinderTests/PartsFinderTests.swift
+++ PartsFinderTests/PartsFinderTests.swift
@@ -5,29 +5,24 @@
 //  Created by Dmitry Aksyonov on 23.08.2021.
 //
 
-import XCTest
 @testable import PartsFinder
+import XCTest
 
 class PartsFinderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+	override func setUpWithError() throws {
+	}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+	override func tearDownWithError() throws {
+	}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+	func testExample() throws {
+	}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+	func testPerformanceExample() throws {
+
+		self.measure {
+		}
+	}
 
 }
